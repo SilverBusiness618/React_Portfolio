9f872b1b24b83f79cf82558a05cb8afb7b734105
sliver <silverbusiness007@gmail.com>
2025-01-22 00:58:59 -0300

    Init Pro

---
+++ src/components/resume/WorkExperience.jsx
@@ -0,0 +1,66 @@
+const WorkExperience = [
+    {
+        id: 1,
+        title: "Software Engineer",
+        company: "Netreo",
+        yearsActive: "May 2022 - Present",
+        information: [
+            "Write modern, performant, maintainable code for a diverse array of internal projects",
+            "Communicate with multi-disciplinary teams of engineers, team leads, and QA Engineers on a daily basis",
+        ],
+    },
+    {
+        id: 2,
+        title: "Full Stack Developer",
+        company: "BearPlex",
+        yearsActive: "May 2020 – March 2022",
+        information: [
+            "Improved and optimized the company's website for a seamless customer experience",
+            "Created, communicated, and managed project plans & project timelines",
+            "Worked to continually meet the expectation of our clients",
+        ],
+    },
+    {
+        id: 3,
+        title: "Software Developer",
+        company: "Innopage",
+        yearsActive: "September 2018 – April 2020",
+        information: [
+            "Planned, developed, tested, deployed, and maintained web applications and mobile applications",
+            "Worked well independently and on a team to solve problems."
+        ],
+    },
+    {
+        id: 4,
+        title: "Junior Software Developer",
+        company: "Innopage",
+        yearsActive: "October 2017 – August 2018",
+        information: [
+            "Served as a dedicated and supportive Intern in a fast-paced environment",
+            "Worked to solve complex problems using the latest in Mobile and Web Technologies.",
+        ],
+    },
+    // {
+    //     id: 5,
+    //     title: "IT Support Assistant",
+    //     company: "Varsity College",
+    //     yearsActive: "February 2015 - May 2015",
+    //     information: [
+    //         "Worked to support the senior support staff with day-to-day tasks.",
+    //         "Assisted with the evaluation and troubleshooting of hardware and software issues.",
+    //         "Maintaining computer Inventory & surplus equipment."
+    //     ],
+    // },
+    // {
+    //     id: 6,
+    //     title: "Student Ambassador",
+    //     company: "Varsity College",
+    //     yearsActive: "December 2014 - February 2015",
+    //     information: [
+    //         "Monitored registration for all prospective student",
+    //         "Assisted parents and prospective student with necessary registration details",
+    //     ],
+    // },
+];
+
+export default WorkExperience;
